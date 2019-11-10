0f5b3b8a028c31c3f7c3ce38a2c8b4e6792ce548
Maximilliann <maxmarciel2@gmail.com>
2019-11-09 23:30:12 -0800

    Created lane prefab type

---
+++ Assets/LanePrefab/adjustLine2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjustLine2 : MonoBehaviour
+{
+    public GameObject primaryAsphalt;
+    float asphaltX;
+    float asphaltY;
+    float asphaltZ;
+    float asphaltZScale;
+    float asphaltXScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UPDATE ALL VARIABLES
+        asphaltX = GameObject.Find("PrimaryAsphalt").transform.localPosition.x;
+        asphaltY = GameObject.Find("PrimaryAsphalt").transform.localPosition.y;
+        asphaltZ = GameObject.Find("PrimaryAsphalt").transform.localPosition.z;
+        asphaltZScale = GameObject.Find("PrimaryAsphalt").transform.localScale.z;
+        asphaltXScale = GameObject.Find("PrimaryAsphalt").transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UPDATE THE Z VARIABLES FOR POSITIONAL UPDATES
+        asphaltZScale = GameObject.Find("PrimaryAsphalt").transform.localScale.z;
+        asphaltZ = -(GameObject.Find("PrimaryAsphalt").transform.localPosition.z);
+
+        //UPDATE THE X VARIABLES FOR POSITIONAL UPDATES
+        asphaltX = -(GameObject.Find("PrimaryAsphalt").transform.localPosition.x);
+        asphaltXScale = GameObject.Find("PrimaryAsphalt").transform.localScale.x;
+
+        //Transforms the scale and position to update with the road.
+        transform.localPosition = new Vector3(-asphaltX, asphaltY, -(asphaltZ + asphaltZScale / 2));
+        transform.localScale = new Vector3(asphaltXScale, 0.3f, 0.15f); //Fix these hard-coded values later.
+
+    }
+}
---
+++ Assets/LanePrefab/adjustSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjustSize : MonoBehaviour
+{
+    public float x = 6.0f;
+    public float y = 0.3f;
+    public float z = 3.0f;
+    public float input = 0;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        transform.localScale = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        transform.localScale = new Vector3(x, y, z + input);
+
+    }
+}
