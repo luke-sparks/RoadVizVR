b718fabaa6a9d2adf0c08b15a93c36c6479a0e9b
Maximilliann <maxmarciel2@gmail.com>
2019-11-17 20:29:20 -0800

    Fixed up the lanes and made them easy to work with, added another simple lane type as a test, and stored the type of vehicles that each lane can use as a public variable so other objects may access them - do not edit them during runtime unless absolutely necessary. Made the lane size relative to line sizes consistent.

---
+++ Assets/LanePrefab/Basic Lane/AdjustAsphaltSize.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustAsphaltSize : MonoBehaviour
+{
+    private float x;
+    private float y;
+    private float z;
+    private float input;
+    public GameObject lane_parent_object;
+    private laneBehavior lane_parent_script;
+
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        x = transform.localScale.x;
+        y = transform.localScale.y;
+        z = transform.localScale.z;
+        lane_parent_script = lane_parent_object.GetComponent<laneBehavior>();
+        input = lane_parent_script.input;
+        transform.localScale = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        input = lane_parent_script.input;
+        transform.localScale = new Vector3(x, y, z + input);
+    }
+}
---
+++ Assets/LanePrefab/Basic Lane/laneBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laneBehavior : MonoBehaviour
+{
+    public float input;
+    public bool carsAllowed   = true;
+    public bool bikesAllowed  = false;
+    public bool trainsAllowed = false;
+    public bool tramsAllowed  = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
