a6ee8b5e38c7c1fc58b7677b95334fd6e7fe9267
Maximilliann <maxmarciel2@gmail.com>
2019-11-24 20:23:34 -0800

    Figured out and uploaded a quick unit test regarding the position of the lane and how the line behaves. Will use this to test the line's position after scaling asphalt in the future.

---
+++ Assets/Tests/AdjustLine1Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class adjustLine1Test
+    {
+        public GameObject lanePrefab = GameObject.Find("Lane_Solid_Lines_Basic");
+        public GameObject asphalt    = GameObject.Find("PrimaryAsphalt");
+        public GameObject line1      = GameObject.Find("Line1");
+        public GameObject line2      = GameObject.Find("Line2");
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void adjustSize_Test()
+        {
+            // Generate a random Z Position
+            bool testPassed = false;
+            float randomZPos = Random.Range(1, 500);
+
+            // Act on moving the asphalt
+            lanePrefab.transform.Translate(new Vector3(0, 0, randomZPos));
+
+            //Retrieve values for the Z position and the expectation.
+            var line1ZPos = line1.transform.position.z;
+            var asphaltZPos = asphalt.transform.position.z;
+            var expectedLine1ZPos = asphaltZPos + 1.575;
+
+            // Verify that the position of the line is correct
+            // Along with the asphalt
+            if (!(line1ZPos > expectedLine1ZPos + 0.01) && !(line1ZPos < expectedLine1ZPos - 0.01))
+            {
+                testPassed = true;
+            }
+
+            Assert.IsTrue(testPassed);
+
+
+        }
+    }
+}
