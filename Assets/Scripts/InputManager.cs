45c4d420f7a6577c782d2568868b736050a0891e
k-davis <kaseydavis216@gmail.com>
2020-02-17 10:51:10 -0800

    InputManager file

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void setViveWandControls()
+    {
+        IVRSystem.GetStringTrackedDeviceProperty(Prop_ManufacturerName_String)
+    }
+
+    void setOculusTouchControls()
+    {
+
+    }
+}
