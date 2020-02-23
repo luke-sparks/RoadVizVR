37aa4bd34d3d9f6259045673aa94ae61209265e2
k-davis <kaseydavis216@gmail.com>
2020-02-23 14:00:36 -0800

    Made settings dialog, adjusted UIManager

---
+++ Assets/UI_Assets/UI_Scripts/GlobalSettingsBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSettingsBehavior : MonoBehaviour, ISceneUIMenu
+{
+    private const float BRIGHTNESS_INCREMENT = 0.5f;
+    private const float FOG_INCREMENT = 0.5f;
+
+    public void setWorkingReference(params GameObject[] objRefs)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void handleBrightnessDecrement()
+    {
+
+    }
+
+    public void handleBrightnessIncrement()
+    {
+
+    }
+
+    public void handleFogDecrement()
+    {
+
+    }
+
+    public void handleFogIncrement()
+    {
+
+    }
+
+    public void handleArchitectureTypeChange()
+    {
+
+    }
+
+
+    public void closeUI()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
