3a007d654d76e28d7dc1ab452c1119b9e434efb8
k-davis <kaseydavis216@gmail.com>
2020-03-02 22:06:18 -0800

    Started Input Manager
    Made some adjustments to control triggers

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,23 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using VRTK;
 
 public class InputManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        evaluateControllerType();
+
+    }
+
+    private void Awake()
+    {
+        GetComponent<VRTK_ControllerEvents>().ButtonOnePressed += DoButtonOnePress;
+    }
+
+    private void DoButtonOnePress(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Button one pressed");
         
     }
 
-    void setViveWandControls()
+    public void evaluateControllerType()
     {
-        IVRSystem.GetStringTrackedDeviceProperty(Prop_ManufacturerName_String)
+        SDK_BaseHeadset.HeadsetType headset = VRTK_DeviceFinder.GetHeadsetType();
+        if (headset == SDK_BaseHeadset.HeadsetType.OculusRift)
+            setOculusTouchControls();
+        else if (headset == SDK_BaseHeadset.HeadsetType.HTCVive)
+            setViveWandControls();
+        else
+            Debug.LogError("Unrecognized controller type, using default input");
     }
 
-    void setOculusTouchControls()
+    private void setViveWandControls()
     {
-
+        Debug.Log("Using Vive Wand Controllers");
     }
+
+    private void setOculusTouchControls()
+    {
+        Debug.Log("Using Oculus Touch Controllers");
+    }
+
 }
