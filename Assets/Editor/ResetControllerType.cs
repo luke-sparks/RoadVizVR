572b38993911de252c7bfd54a0fc8dca02e0246d
k-davis <kaseydavis216@gmail.com>
2020-03-02 22:06:18 -0800

    Started Input Manager
    Made some adjustments to control triggers

---
+++ Assets/Editor/ResetControllerType.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(InputManager))]
+public class NewBehaviourScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        InputManager inputManager = (InputManager)target;
+
+        if (GUILayout.Button("Reevaluate Controller Type"))
+        {
+            inputManager.evaluateControllerType();
+        }
+    }
+}
