083c0392014f563bd6a98f04b5670ee87f12e695
k-davis <kaseydavis216@gmail.com>
2020-03-22 17:10:51 -0700

    Added edit stripe sub menus

--- Assets/UI_Assets/UI_Scripts/EditStripeBehavior.cs
+++ Assets/UI_Assets/UI_Scripts/EditStripeBehavior.cs
@@ -6,14 +6,24 @@ using UnityEngine.UI;
 public class EditStripeBehavior : MonoBehaviour
 {
     /* EditStripeBehavior does not inherit from ISceneUIMenu 
+<<<<<<< HEAD
      *  because it is a submenu. But it does have init for the stripe refs
+=======
+     *  because it is a submenu
+>>>>>>> Added edit stripe sub menus
      */
     public string panelLabel;
     private GameObject stripeReference;
 
+<<<<<<< HEAD
     public void init(params GameObject[] stripeRefs)
     {
         stripeReference = stripeRefs[0];
+=======
+    public void setWorkingReference(GameObject stripeRef)
+    {
+        stripeReference = stripeRef;
+>>>>>>> Added edit stripe sub menus
     }
 
     public void handlePatternChange()
@@ -26,6 +36,21 @@ public class EditStripeBehavior : MonoBehaviour
         Debug.Log("Color change selected.");
     }
 
+<<<<<<< HEAD
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+>>>>>>> Added edit stripe sub menus
     // Called when inspector variables change
     void OnValidate()
     {
