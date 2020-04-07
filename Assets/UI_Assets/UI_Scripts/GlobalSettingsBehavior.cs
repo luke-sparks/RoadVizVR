a74573a53f5b757d810dd2f58498fcf971a6b3f4
k-davis <kaseydavis216@gmail.com>
2020-04-06 22:04:44 -0700

    attached brightness controls to ui

--- Assets/UI_Assets/UI_Scripts/GlobalSettingsBehavior.cs
+++ Assets/UI_Assets/UI_Scripts/GlobalSettingsBehavior.cs
@@ -5,30 +5,37 @@ using UnityEngine.UI;
 
 public class GlobalSettingsBehavior : MonoBehaviour, ISceneUIMenu
 {
-    private const float BRIGHTNESS_INCREMENT = 0.5f;
+    private const float BRIGHTNESS_INCREMENT = 0.1f;
     private const float FOG_INCREMENT = 12.0f;
     private fogControl fogController;
+    private brightnessControl lightController;
+
     
     public void setWorkingReference(params GameObject[] objRefs)
     {
         fogController = GameObject.Find("fogController").GetComponent<fogControl>();
+        lightController = GameObject.Find("Lights/Solar Light").GetComponent<brightnessControl>();
         updateUIValues();
     }
 
     private void updateUIValues()
     {
-        transform.Find("FogLevelControls/Background/FogField").GetComponent<Text>().text = fogController.getFogDistance().ToString();
+        transform.Find("FogLevelControls/BrightnessBackground/BrightnessField").GetComponent<Text>().text = lightController.getBrightness().ToString();
+        transform.Find("BrightnessLevelControls/Background/FogField").GetComponent<Text>().text = fogController.getFogDistance().ToString();
         transform.Find("ArchitectureTypeControls/ArchType").GetComponent<Dropdown>().value = GameObject.Find("buildings").GetComponent<buildings>().getBuildingIndex();
+
     }
 
     public void handleBrightnessDecrement()
     {
-        
+        lightController.setBrightness(lightController.getBrightness() - BRIGHTNESS_INCREMENT);
+        updateUIValues();
     }
 
     public void handleBrightnessIncrement()
     {
-
+        lightController.setBrightness(lightController.getBrightness() + BRIGHTNESS_INCREMENT);
+        updateUIValues();
     }
 
     public void handleFogDecrement()
