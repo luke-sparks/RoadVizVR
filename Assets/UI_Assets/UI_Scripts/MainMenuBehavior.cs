480ed5ee3d652435478d2cd78f01ccffa4c19ab5
k-davis <kaseydavis216@gmail.com>
2020-04-15 16:07:21 -0700

    Main menu functionality

---
+++ Assets/UI_Assets/UI_Scripts/MainMenuBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void handleQuitApplication()
+    {
+        Debug.Log("Quitting application");
+        Application.Quit();
+    }
+
+    public void handleStartDesigning()
+    {
+        Debug.Log("Switching to Dev Env scene");
+        SceneManager.LoadScene("DevelopmentEnvironment");
+    }
+}
