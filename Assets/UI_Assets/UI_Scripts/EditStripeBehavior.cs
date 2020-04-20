782b4289e6cb36a5b9ebfcc60c62c5f2ea9f4487
k-davis <kaseydavis216@gmail.com>
2020-04-19 20:22:24 -0700

    Repaired forgotten stripes ui

--- Assets/UI_Assets/UI_Scripts/EditStripeBehavior.cs
+++ Assets/UI_Assets/UI_Scripts/EditStripeBehavior.cs
@@ -26,18 +26,6 @@ public class EditStripeBehavior : MonoBehaviour
         Debug.Log("Color change selected.");
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // Called when inspector variables change
     void OnValidate()
     {
