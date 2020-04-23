855f1a306f0ddd10a7bf062cd89a906e8bb085a5
k-davis <kaseydavis216@gmail.com>
2020-04-23 16:54:34 -0700

    deactivates load when no files are available

--- Assets/UI_Assets/UI_Scripts/MainMenuBehavior.cs
+++ Assets/UI_Assets/UI_Scripts/MainMenuBehavior.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    private Dropdown roadDropdown;
     // Start is called before the first frame update
     void Start()
     {
-        
+        roadDropdown = gameObject.transform.Find("LoadRoadControls/RoadNameDropdown").GetComponent<Dropdown>();
+        // add lane types to dropdown, then set current active
+        roadDropdown.ClearOptions();
+
+        List<string> roadFileNames = new List<string>(RoadVizSaveSystem.getFilenames());
+        if(roadFileNames.Count == 0)
+        {
+            gameObject.transform.Find("LoadRoadControls/LoadButton").GetComponent<Button>().interactable = false; ;
+        }
+
+        roadDropdown.AddOptions(new List<string>(RoadVizSaveSystem.getFilenames()));  
     }
 
     // Update is called once per frame
@@ -36,6 +49,7 @@ public class MainMenuBehavior : MonoBehaviour
 
     public void handleLoadDesign()
     {
-
+        SceneManager.LoadScene("DevelopmentEnvironment");
+        RoadVizSaveSystem.loadRoadFromMemory(RoadVizSaveSystem.getFilenames()[roadDropdown.value]);
     }
 }
